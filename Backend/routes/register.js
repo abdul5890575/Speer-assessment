98cb7a14007e1c20b9e2e96ccb85d03a42aee172
abdul5890575 <abdulrehman.shoukat@gmail.com>
2021-06-01 18:45:07 -0400

    register route with validation

---
+++ Backend/routes/register.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const {} = require('../helpers/dataHelpers');
+
+module.exports = ({ 
+    getUserByEmail,
+    addUser
+}) => {
+    /* GET users listing. */
+
+
+    router.post('/', (req, res) => {
+
+        const {
+            first_name,
+            last_name,
+            email,
+            password
+        } = req.body;
+
+        if (req.body.email === '' || req.body.password === '' || req.body.username === '') {
+            res.json({
+                msg: 'Username, Email or Password empty'
+            })
+        } else {
+            getUserByEmail(email)
+            .then(user => {
+                if (user) {
+                    res.json({
+                        msg: 'Sorry, a user account with this email already exists'
+                    });
+                } else {
+                    return addUser(first_name, last_name, email, password)
+                }
+            })
+            .then(newUser => res.json(newUser))
+            .catch(err => res.json({
+                error: err.message
+            }));
+        }
+    })
+
+    return router;
+};
